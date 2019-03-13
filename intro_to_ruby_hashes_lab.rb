c256f2bbb39b8ba2f25599473c548f035cf1bc67
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-13 20:49:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,13 +31,13 @@ def monopoly_with_third_tier
 	    :names => {
 	      :reading_railroad => {},
 	      :pennsylvania_railroad => {},
-	      :b_and_railroad => {},
+	      :b_and_o_railroad => {},
 	      :shortline => {}
 	    },
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
-	      :two_peices_owned => 50,
-	      :three_peices_owned => 100,
+	      :two_pieces_owned => 50,
+	      :three_pieces_owned => 100,
 	      :four_pieces_owned => 200
 	    }
 	  }
